3c03f452e8402024a3186055efc0b7c0feab76c7
mofthecross <mofthecross@gmail.com>
2016-06-24 22:22:58 -0700

    made App a stateful class component and render one videoPlayer component and videoPlayerList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
 //HELLO
 //hello again lets see if this works
-var App = () => (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onVideoTitleClick (evt) {
+    console.log('Firing!');
+    this.setState( {
+      done: !this.state.done
+    })
+  }
+  render() { 
+  
+  return (
    <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer video={window.exampleVideoData}/>
+      <VideoPlayer video={window.exampleVideoData[0]}/>
     </div>
     <div className="col-md-5">
       <VideoList videos={window.exampleVideoData}/>
     </div>
   </div>
-);
+  );
+  } 
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
